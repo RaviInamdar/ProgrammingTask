432ee42abafb6f6010303e5d8b65de3f4c65cef0
System Administrator <root@Ravis-Air.fios-router.home>
2021-07-24 12:27:00 -0400

    trying different method

--- ProgrammingTask.js
+++ ProgrammingTask.js
@@ -15,6 +15,7 @@ const api_names =
   fetch(), a (relatively) new JS way of retrieving files. We will use this
   function to call both api's.
 */
+/*
 async function retrieveAPI(url){
   // retrieves response from the URl using fetch()
 	const response = await fetch(url);
@@ -24,39 +25,25 @@ async function retrieveAPI(url){
   // we want to return this data as we want to save it outside of this fxn
   return data;
 }
-
-// use the retrieveAPI() function to get and save names.
-var apiAgeData = retrieveAPI(api_ages) || [];
-var apiNameData = retrieveAPI(api_names) || [];
-
-console.log('ageData is', apiAgeData);
-console.log('nameData is', apiNameData);
-
-/*
-  We are going to do 2 things here.
-  - convert all data in api_ages and api_names into their own separate ageData
-  - combine data from both tables into one combination table, where our
-  assumption is that id is a field that exists in EVERY line as this is the key
-
 */
 
-console.log("destructing age and name data...");
-
-destructAges(apiAgeData);
-destructNames(apiNameData);
-
-function destructAges(api){
-  console.log("ID | AGE")
-  for(let i = 0; i < api.length; i++){
-    console.log(api[i].id, " | " ,api[i].age);
-    console.log("\n");
-  }
+function createTable(url){
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log("data is ", data);
+  })
 }
 
-function destructNames(api){
-  console.log("ID | FIRST NAME | LAST NAME")
-  for(let i = 0; i < api.length; i++){
-    console.log(api[i].id, " | ", api[i].firstName, " | ", api[i].lastName);
-    console.log("\n");
-  }
-}
+console.log("api ages");
+createTable(api_ages);
+
+console.log("api names");
+createTable(api_names);
+
+// use the retrieveAPI() function to get and save names.
+// var apiAgeData = retrieveAPI(api_ages);
+// var apiNameData = retrieveAPI(api_names);
+
+// console.log('ageData is', apiAgeData);
+// console.log('nameData is', apiNameData);
